4932d92f9ac9758933b708f8da87cdc96ae4512b
Jacob Emcken <jacob@emcken.dk>
2019-06-28 10:45:53 +0200

    Fix project URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject fullspectrum/aws-lambda-adapters "0.1.0-SNAPSHOT"
   :description "Easy AWS Lambda integration."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/full-spectrum/aws-lambda-adapters"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.0"]
