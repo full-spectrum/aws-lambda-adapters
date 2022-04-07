c40eb1532618d2fb6d830f76576121bd43d6ccb2
Jacob Emcken <jacob@emcken.dk>
2022-04-07 12:21:58 +0200

    Release version 1.0.0

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject fullspectrum/aws-lambda-adapters "0.1.0-SNAPSHOT"
+(defproject fullspectrum/aws-lambda-adapters "1.0.0"
   :description "Easy AWS Lambda integration."
   :url "https://github.com/full-spectrum/aws-lambda-adapters"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/data.json "0.2.6"]])
+                 [org.clojure/data.json "2.4.0"]])
